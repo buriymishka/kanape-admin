06d7bfa74be6345bfda9c27f66247c8b2b6b4009
Mikhail Uglianskii <mikhail.uglianskii@advcake.com>
2022-01-03 17:52:00 +0200

    db

--- server/db.js
+++ server/db.js
@@ -4,6 +4,5 @@ const keys = require('./keys')
 module.exports = new Sequelize(keys.DB_DATABASE, keys.DB_USER, keys.DB_PASSWORD, {
   host: keys.DB_HOST,
   port: keys.DB_PORT,
-  dialect: 'postgres',
-  native: true
+  dialect: 'postgres'
 })
