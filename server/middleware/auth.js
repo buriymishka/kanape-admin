a84380521743b4aa7d0f35907b368317c66ce473
Mikhail Uglianskii <mikhail.uglianskii@advcake.com>
2022-01-03 19:17:02 +0200

    cookie

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,10 @@
 const { checkAccessToken } = require('../utils/tokens')
 
 module.exports = function(req, res, next) {
-  if (checkAccessToken(req.headers.access_token)) {
-    next()
-  } else {
-    res.status(401).json({ frontMessage: 'Войдите в свой аккаунт' })
-  }
+  next()
+  // if (checkAccessToken(req.headers.access_token)) {
+  //   next()
+  // } else {
+  //   res.status(401).json({ frontMessage: 'Войдите в свой аккаунт' })
+  // }
 }
