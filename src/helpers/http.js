4056383162fe558862c4bd52b6fc1c3113412b7d
Mikhail Uglianskii <mikhail.uglianskii@advcake.com>
2022-01-15 13:50:35 +0200

    done

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -3,7 +3,7 @@ import router from '@/router'
 import { getAC, setAC } from '@/tokens'
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:3000/api/',
+  baseURL: process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:8089/api/',
   withCredentials: true
 })
 
